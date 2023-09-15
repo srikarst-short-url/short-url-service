09f00b9c336ad36b0617544837241c51007ac777
srikarst <srikarsuri@gmail.com>
2023-09-15 14:14:24 +0000

    Remove metadata refs of sdk

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
 import mongoose from "mongoose";
-import AWS from "aws-sdk";
 import { app } from "./app";
 
 const start = async () => {
   try {
-    var meta = new AWS.MetadataService();
-
-    meta.request("/latest/meta-data/instance-id", function (err, data) {
-      console.log(data);
-    });
     await mongoose.connect(
       "mongodb+srv://srikarsuri:Srikar10$@cluster0.a0wwm8w.mongodb.net/?retryWrites=true&w=majority"
     );
