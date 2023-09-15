3f94976d46c3102cd12d1bb9fb209cd4eadea0ab
srikarst <srikarsuri@gmail.com>
2023-09-15 10:10:44 +0000

    Add log for instance id with metadata

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express";
+import awsInstanceMetadata from "aws-instance-metadata";
 
 const router = express.Router();
 
 router.get("/", async (req: Request, res: Response) => {
+  let ip = await awsInstanceMetadata.fetch("instance-id");
+  console.log(ip);
   res.send(`Welcome to URL shortener service`);
 });
 
