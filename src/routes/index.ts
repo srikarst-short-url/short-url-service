5984db6fedc27969d75702f679841e4b85068604
srikarst <srikarsuri@gmail.com>
2023-09-15 07:38:07 +0000

    Show host ip address on root req

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from "express";
+import ip from "ip"
 
 const router = express.Router();
 
 router.get("/", async (req: Request, res: Response) => {
-  res.send("Welcome to URL shortener service");
+  res.send(`Welcome to URL shortener service, ip is ${ip.address()}`);
 });
 
 export { router as indexRouter };
